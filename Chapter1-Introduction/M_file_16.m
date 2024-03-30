38a29fb79d3e904ba9f226fa87719048dc1ed918
Camille <144267933+Ellimaaac@users.noreply.github.com>
2024-03-30 14:24:53 +0100

    Update M_file_16.m

--- Chapter1-Introduction/M_file_16.m
+++ Chapter1-Introduction/M_file_16.m
@@ -1,19 +1,18 @@
-% Lire le fichier audio d'entrée dans le vecteur signal et la fréquence d'échantillonnage FS
+% Lire le fichier audio 
 [signal, FS] = audioread("punk.wav");
 
-% Assurez-vous que signal est un vecteur
 if size(signal, 2) > 1
     signal = signal(:, 1); % Extrait la première colonne si signal est une matrice
 end
 
-% Afficher le signal dans le premier sous-graphique
+% Afficher le signal 
 subplot(2,1,1);
 plot(signal);
 title('Signal Temporel');
 xlabel('Temps (échantillons)');
 ylabel('Amplitude');
 
-% Calculer et afficher le spectrogramme dans le deuxième sous-graphique
+% Calculer et afficher le spectrogramme 
 subplot(2,1,2);
 spectrogram(signal, 256, 256-128, 512, FS, 'yaxis');
 title('Spectrogramme');
